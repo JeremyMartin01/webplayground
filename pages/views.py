c9202de8a757b590020d5f37d2729ae4421012ba
Jeremías <martinbernardinoj@gmail.com>
2024-01-10 20:03:17 -0600

    cambios 1/10/2024

--- pages/views.py
+++ pages/views.py
@@ -1,27 +1,46 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, updateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect
 from .models import Page
+from .forms import PageForm
+
+class StaffRequiredMixin(object):
+    """
+    Este mixin requerirá que el usuario sea miembro del staff
+    """
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 # Create your views here.
-#Para cuando tengamos que devolver la lista de las instancias de un modelo
+    #Para cuando tengamos que devolver la lista de las instancias de un modelo
 #ListView = Devolver una lista genérica y mostrarlo en un template
 class PageListView(ListView):
-    model=Page
-   
+    model = Page
 #Para cuando tengamos que devolver unicamente una instancia de un modelo.
 class PageDetailView(DetailView):
-   model=Page
+    model = Page
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageCreate(CreateView):
-    model=Page
-    fields = ['title', 'content', 'order']
+    model = Page
+    form_class = PageForm
     success_url = reverse_lazy('pages:pages')
- 
 #Las vistas Delete y Update pero estas tenemos una primary key para recuperar las instancias.
+@method_decorator(staff_member_required, name='dispatch')
+class PageUpdate(UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name_suffix = '_update_form'
 
-class PageUpdate(CreateView):
-    model=Page
-    fields = ['title', 'content', 'order']
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+@method_decorator(staff_member_required, name='dispatch')
+class PageDelete(DeleteView):
+    model = Page
     success_url = reverse_lazy('pages:pages')
