2e849398a84cdc42be357301e73cc6165f3bf552
Jeremías <martinbernardinoj@gmail.com>
2024-01-21 21:21:41 -0600

    Se agregó app registration, fix setting, decoradores, crud

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,6 @@
+from typing import Any
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -5,22 +8,25 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import redirect
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
 
 class StaffRequiredMixin(object):
-    """
-    Este mixin requerirá que el usuario sea miembro del staff
-    """
+    # Este mixin requerirá que el usuario sea miembro del staff
     @method_decorator(staff_member_required)
-    def dispatch(self, request, *args, **kwargs):
-        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
-
+    def dispatch(self, request,*args, **kwargs):
+        #if not request.user.is_staff:
+        #    return redirect(reverse_lazy('admin:login'))
+        return super(PageCreate, self).dispatch(request, *args, **kwargs)
+    
 # Create your views here.
     #Para cuando tengamos que devolver la lista de las instancias de un modelo
 #ListView = Devolver una lista genérica y mostrarlo en un template
 class PageListView(ListView):
     model = Page
+
+@method_decorator(staff_member_required, name='dispatch')
 #Para cuando tengamos que devolver unicamente una instancia de un modelo.
 class PageDetailView(DetailView):
     model = Page
@@ -30,6 +36,7 @@ class PageCreate(CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
+
 #Las vistas Delete y Update pero estas tenemos una primary key para recuperar las instancias.
 @method_decorator(staff_member_required, name='dispatch')
 class PageUpdate(UpdateView):
